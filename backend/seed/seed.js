388c0dd91b4be0e2e886e0a782075492d3d51717
Maxime Castres <mcastres@student.42.fr>
2020-07-27 11:12:02 +0200

    Fix url issue

--- backend/seed/seed.js
+++ backend/seed/seed.js
@@ -1,7 +1,6 @@
 module.exports = {
   categories: [
     {
-      "id": 1,
       "name": "news",
       "slug": "news",
       "created_by": null,
@@ -10,7 +9,6 @@ module.exports = {
       "updated_at": "2020-07-08T13:33:04.646Z",
     },
     {
-      "id": 2,
       "name": "tech",
       "slug": "tech",
       "created_by": null,
@@ -19,7 +17,6 @@ module.exports = {
       "updated_at": "2020-07-13T14:44:31.107Z",
     },
     {
-      "id": 3,
       "name": "food",
       "slug": "food",
       "created_by": null,
@@ -28,7 +25,6 @@ module.exports = {
       "updated_at": "2020-07-16T08:09:53.954Z",
     },
     {
-      "id": 4,
       "name": "nature",
       "slug": "nature",
       "created_by": null,
@@ -37,7 +33,6 @@ module.exports = {
       "updated_at": "2020-07-16T09:40:38.488Z",
     },
     {
-      "id": 5,
       "name": "story",
       "slug": "story",
       "created_by": null,
@@ -112,7 +107,6 @@ module.exports = {
   ],
   articles: [
     {
-      "id": 1,
       "title": "The internet's Own boy",
       "content": "# The Story of Aaron Swartz\n\nAaron Hillel Swartz (November 8, 1986 – January 11, 2013) was an American computer programmer, entrepreneur, writer, political organizer, and Internet hacktivist. He was involved in the development of the web feed format RSS, the Markdown publishing format, the organization Creative Commons, and the website framework web.py, and was a co-founder of the social news site [Reddit](https://reddit.com). He was given the title of co-founder of Reddit by Y Combinator owner [Paul Graham](https://en.wikipedia.org/wiki/Paul_Graham_(programmer)) after the formation of Not a Bug, Inc. (a merger of Swartz's project Infogami and Redbrick Solutions, a company run by Alexis Ohanian and Steve Huffman).\n\nSwartz's work also focused on civic awareness and activism. He helped launch the Progressive Change Campaign Committee in 2009 to learn more about effective online activism. In 2010, he became a research fellow at Harvard University's Safra Research Lab on Institutional Corruption, directed by Lawrence Lessig. He founded the online group Demand Progress, known for its campaign against the Stop Online Piracy Act.\n\n\n<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/PFQGjEJ9PEc\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>\n\n\nIn 2011, Swartz was arrested by Massachusetts Institute of Technology (MIT) police on state breaking-and-entering charges, after connecting a computer to the MIT network in an unmarked and unlocked closet, and setting it to download academic journal articles systematically from JSTOR using a guest user account issued to him by MIT. Federal prosecutors later charged him with two counts of wire fraud and eleven violations of the Computer Fraud and Abuse Act,[14] carrying a cumulative maximum penalty of $1 million in fines, 35 years in prison, asset forfeiture, restitution, and supervised release.\n\nSwartz declined a plea bargain under which he would have served six months in federal prison. Two days after the prosecution rejected a counter-offer by Swartz, he was found dead in his Brooklyn apartment, where he had hanged himself.\n\nIn 2013, Swartz was inducted posthumously into the [Internet Hall of Fame](https://www.internethalloffame.org//).",
       "publishedAt": "2020-07-16T10:00:00.000Z",
@@ -141,7 +135,6 @@ module.exports = {
       "image": null
     },
     {
-      "id": 2,
       "title": "What's inside a Black Hole",
       "content": "Well, we don't know yet...",
       "publishedAt": "2020-07-16T10:00:00.000Z",
@@ -170,7 +163,6 @@ module.exports = {
       "image": null
     },
     {
-      "id": 3,
       "title": "This shrimp is awesome",
       "content": "# Badass\n\nMantis shrimps, or stomatopods, are marine crustaceans of the order Stomatopoda. Some species have specialised calcified \"*clubs*\" that can strike with **great power**, while others have sharp forelimbs used to capture prey. They branched from other members of the class Malacostraca around 340 million years ago. Mantis shrimps typically grow to around 10 cm (3.9 in) in length. A few can reach up to 38 cm (15 in). The largest mantis shrimp ever caught had a length of 46 cm (18 in); it was caught in the Indian River near Fort Pierce, Florida, in the United States. A mantis shrimp's carapace (the bony, thick shell that covers crustaceans and some other species) covers only the rear part of the head and the first four segments of the thorax. Varieties range in color from shades of brown to vivid colors, with more than 450 species of mantis shrimps being known. They are among the most important predators in many shallow, tropical and subtropical marine habitats. However, despite being common, they are poorly understood, as many species spend most of their lives tucked away in burrows and holes.\n\nCalled \"sea locusts\" by ancient Assyrians, \"prawn killers\" in Australia, and now sometimes referred to as \"thumb splitters\"—because of the animal's ability to inflict painful gashes if handled incautiously—mantis shrimps have powerful claws that are used to attack and kill prey by spearing, stunning, or dismembering.\n\n<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/CW8NUCPLE1c\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>\n\nMantis shrimps have, like mantis, ravishing legs. But theirs are so resilient that they inspire the military to make the armor of the future. Above all, their speed and striking power are the responsibility of the superpowers. As the comic book The Oatmeal recounts, the squid's ravishing legs move at the speed of a bullet fired by a 22 caliber and can strike a prey in 1/3000 of a second, with a force of 1,500 Newtons. One tenth of that speed, deployed by a human arm, \"would be enough to send a baseball into orbit.\"\n\nA speed such as it boils water around ravishing legs and creates a shock wave capable of killing a prey missed by the strike.\n\nKept in an aquarium, crustaceans make carnage by dismembering and devouring crabs, shrimps, octopuses, snails and shells (whose shells they break). They would even be able to break the windows of overly fragile aquariums.",
       "publishedAt": "2020-07-16T10:00:00.000Z",
@@ -199,7 +191,6 @@ module.exports = {
       "image": null
     },
     {
-      "id": 4,
       "title": "A bug is becoming a meme on the internet",
       "content": "It's the story of a user named **Omer Barnir** who reported a bug in 2005 on the MySQL [bug report platform](https://bugs.mysql.com/)\n\nBut the thing is that Omer never get an answer. 15 years later, the bug has never been fix and people are starting to make fun out of it. We let you take a look at the conversation [here](https://bugs.mysql.com/bug.php?id=11472), it's pretty funny",
       "publishedAt": "2020-07-16T10:00:00.000Z",
@@ -228,7 +219,6 @@ module.exports = {
       "image": null
     },
     {
-      "id": 5,
       "title": "Beautiful picture",
       "content": "This is an empty blog post with a beautiful picture",
       "publishedAt": "2020-07-16T10:00:00.000Z",
@@ -257,7 +247,6 @@ module.exports = {
       "image": null
     },
     {
-      "id": 6,
       "title": "We love Pizza",
       "content": "We love pizza at Strapi, what about you?",
       "publishedAt": "2020-07-16T10:00:00.000Z",
