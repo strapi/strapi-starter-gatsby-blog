959c28c1e24a6a04819fcdb97f032ac1501a5288
Marvin Frachet <marvin.frachet@protonmail.com>
2021-01-18 19:35:08 +0100

    refactoring(gatsby-node): moving to the gatsby file system API (#14)
    * wip
    
    * Moving categories to the file system API
    
    * Making eslint targetting only src
    
    Co-authored-by: mfrachet <marvin@gatsbyjs.com>

--- frontend/src/pages/article/{StrapiArticle.slug}.js
+++ frontend/src/pages/article/{StrapiArticle.slug}.js
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 import Moment from "react-moment";
-import Layout from "../components/layout";
+import Layout from "../../components/layout";
 import Markdown from "react-markdown";
 
 export const query = graphql`
